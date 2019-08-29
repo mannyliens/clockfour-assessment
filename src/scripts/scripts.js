a87edff6c6480c96838dccdb6db1a0823db63040
bob <bob@gmail.com>
2019-08-29 12:35:40 -0400

    Fixed jquery id issue & bp3 revision

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('#faq').on('click', function() {
-        $('#faq-item').toggle();
+        $('.faq-item').toggle();
         $(".fa-caret-right, .fa-caret-down").toggleClass("fa-caret-right fa-caret-down");
     })
 });
