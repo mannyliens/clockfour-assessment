1bae7ad154bf30517c72be0a93e614e840cad27d
bob <bob@gmail.com>
2019-08-29 09:37:24 -0400

    Updated files per breakpoints

---
+++ src/scripts/scripts.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('#faq').on('click', function() {
+        $('#faq-item').toggle();
+        $(".fa-caret-right, .fa-caret-down").toggleClass("fa-caret-right fa-caret-down");
+    })
+});
